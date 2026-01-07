51a5d6425fbfb9a3cddd07c5eeb7c687e0266d9b
Trader Trade <99019384+X2C02mo@users.noreply.github.com>
2026-01-07 20:11:02 +0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,19 +1,14 @@
-// api/webhook.js
 const { bot } = require("../support-bot");
 
 module.exports = async (req, res) => {
-  try {
-    if (req.method !== "POST") {
-      res.status(200).send("OK");
-      return;
-    }
+  // отвечаем мгновенно
+  res.status(200).send("OK");
 
+  try {
+    if (req.method !== "POST") return;
     const update = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
     bot.processUpdate(update);
-
-    res.status(200).send("OK");
   } catch (e) {
     console.error("[api/webhook] error:", e);
-    res.status(200).send("OK"); // Telegram должен получить 200
   }
 };
